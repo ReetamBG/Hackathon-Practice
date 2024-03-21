2a6b4172c51b1363e98e01dc7fd4cf545c27adf0
ReetamBG <r33t4m@gmail.com>
2024-03-21 20:15:24 +0530

    chat app

---
+++ chat/index.js
@@ -0,0 +1,15 @@
+import { Server } from "socket.io";
+const io = new Server(server);
+io.listen(8000);
+
+const users = {}
+
+io.on("connection", socket => {
+    socket.on("new-user-joined", name => {
+        users[socket.id] = name;
+        socket.broadcast.emit("user-joined", name)
+    })
+
+    socket.on('send', message => {
+        socket.broadcast.emit("recieve", {message : message, name : user[socket.id]})
+})
