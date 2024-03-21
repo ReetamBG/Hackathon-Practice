6c6d0c220e6fd460eba0863f3b68dd616bda4b79
Shaswata <gshaswata1@gmail.com>
2024-03-21 21:22:52 +0530

    map resizing done

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,6 +1,6 @@
-const map = document.querySelector(".map")
+const map = document.querySelector(".map-container")
 
 navigator.geolocation.getCurrentPosition(position => {
     const {latitude, longitude} = position.coords
-    map.innerHTML = '<iframe width="700" height="300" src="https://maps.google.com/maps?q='+latitude+', '+longitude+'&amp;z=15&amp;output=embed"></iframe>'
+    map.innerHTML = '<iframe width="100%" height="100%" src="https://maps.google.com/maps?q='+latitude+', '+longitude+'&amp;z=15&amp;output=embed"></iframe>'
 })
